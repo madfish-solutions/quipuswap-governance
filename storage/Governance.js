b74d8940e4955b5621d2692fcdf3b4a6b3009012
cyace84 <cyace84@gmail.com>
2021-07-07 15:33:00 +0300

    add qnot_address

--- storage/Governance.js
+++ storage/Governance.js
@@ -19,4 +19,5 @@ module.exports = {
   proposal_config: proposalConfig,
   pending_owner: null,
   temp_proposal_cache: MichelsonMap.fromLiteral({}),
+  qnot_address: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
 };

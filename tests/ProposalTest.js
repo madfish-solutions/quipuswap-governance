e60eb79dedd8d07d5a57d1cefc7feb6e7c48bda3
cyace84 <cyace84@gmail.com>
2021-07-08 01:06:18 +0300

    full proposal tests

--- tests/ProposalTest.js
+++ tests/ProposalTest.js
@@ -1,100 +1,87 @@
 const { rejects, strictEqual } = require("assert");
-const { Tezos, signerAlice } = require("./utils/cli");
-
-const { confirmContract } = require("./utils/confirmation");
-
-async function awaitNewProposal(contract, beforeCount) {
-  do {
-    var storage = await contract.storage();
-  } while (storage.id_count.toNumber() === beforeCount);
-}
-
+const { Tezos, signerAlice, alice } = require("./utils/cli");
 const { migrate } = require("../scripts/helpers");
+
+function dayToSec(day) {
+  return day * 86400;
+}
 
 describe("Proposal test", async function () {
   Tezos.setSignerProvider(signerAlice);
   let contract;
+  let link = Buffer.from("ipfsLink", "ascii").toString("hex");
 
   before(async () => {
     try {
-      const storage = require("../storage/Governance");
-      let deployedContract = await migrate(Tezos, "Governance", storage);
-      contract = await confirmContract(Tezos, deployedContract);
+      const { storages } = require("./storage/storage");
+      let deployedContract = await migrate(
+        Tezos,
+        "Governance",
+        storages["defaultStorage"],
+      );
+      contract = await Tezos.contract.at(deployedContract);
+      const { address } = require("../scripts/sandbox/fa2_latest.json");
+      fa2_contract = await Tezos.contract.at(address);
+      const update_op = await fa2_contract.methods
+        .update_operators([
+          {
+            add_operator: {
+              owner: alice.pkh,
+              operator: deployedContract,
+              token_id: 0,
+            },
+          },
+        ])
+        .send();
+      await update_op.confirmation();
     } catch (e) {
       console.log(e);
     }
   });
 
-  let ipfs_link = Buffer.from("ipfsLink", "ascii").toString("hex");
-  let forum_link = Buffer.from("forumLink", "ascii").toString("hex");
-
   describe("Testing entrypoint: new_proposal", async function () {
     it("Minimum voting period 3 days", async function () {
       await rejects(
-        contract.methods.new_proposal(ipfs_link, forum_link, 2).send(),
-        (err) => {
-          strictEqual(err.message, "Minimum voting period 3 days");
+        contract.methods.new_proposal(link, link, 1, 1).send(),
+        err => {
+          strictEqual(err.message, "Gov/small-voting-perod");
           return true;
-        }
+        },
       );
     });
     it("Maxium voting period 30 days", async function () {
       await rejects(
-        contract.methods.new_proposal(ipfs_link, forum_link, 31).send(),
-        (err) => {
-          strictEqual(err.message, "Maxium voting period 30 days");
+        contract.methods.new_proposal(link, link, dayToSec(31), 0).send(),
+        err => {
+          strictEqual(err.message, "Gov/long-voting-perod");
           return true;
-        }
-      );
-    });
-    it("Successful new proposal", async function () {
-      await contract.methods.new_proposal(ipfs_link, forum_link, 3).send();
-      await awaitNewProposal(contract, 0);
-
-      let storage = await contract.storage();
-      strictEqual(1, storage.id_count.toNumber());
-    });
-  });
-  describe("Testing entrypoint: New_deferred_proposal", async function () {
-    it("Minimum voting period 3 days", async function () {
-      await rejects(
-        contract.methods
-          .new_deferred_proposal(ipfs_link, forum_link, 2, 5)
-          .send(),
-        (err) => {
-          strictEqual(err.message, "Minimum voting period 3 days");
-          return true;
-        }
-      );
-    });
-    it("Maxium voting period 30 days", async function () {
-      await rejects(
-        contract.methods
-          .new_deferred_proposal(ipfs_link, forum_link, 31, 5)
-          .send(),
-        (err) => {
-          strictEqual(err.message, "Maxium voting period 30 days");
-          return true;
-        }
+        },
       );
     });
     it("Deferral can't be longer than 30 days", async function () {
       await rejects(
         contract.methods
-          .new_deferred_proposal(ipfs_link, forum_link, 4, 31)
+          .new_proposal(link, link, dayToSec(4), dayToSec(31))
           .send(),
-        (err) => {
-          strictEqual(err.message, "Deferral can't be longer than 30 days");
+        err => {
+          strictEqual(err.message, "Gov/long-deferral");
           return true;
-        }
+        },
       );
     });
-    it("Successful new deferred proposal", async function () {
-      await contract.methods
-        .new_deferred_proposal(ipfs_link, forum_link, 3, 3)
+    it("Successful new proposal", async function () {
+      let op = await contract.methods
+        .new_proposal(link, link, dayToSec(4), 0)
         .send();
-      await awaitNewProposal(contract, 1);
-
+      await op.confirmation();
+      let storage = await contract.storage();
+      strictEqual(1, storage.id_count.toNumber());
+    });
+    it("Successful new deferred proposal", async function () {
+      let op = await contract.methods
+        .new_proposal(link, link, dayToSec(4), dayToSec(15))
+        .send();
+      await op.confirmation();
       let storage = await contract.storage();
       strictEqual(2, storage.id_count.toNumber());
     });

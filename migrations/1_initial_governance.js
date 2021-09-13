d35175d29472413d97e58b283b75bd5dc0206de9
cyace84 <cyace84@gmail.com>
2021-09-13 19:26:19 +0300

    update migrations

--- migrations/1_initial_governance.js
+++ migrations/1_initial_governance.js
@@ -1,18 +1,23 @@
 const { migrate } = require("../scripts/helpers");
 const governanceStorage = require("../storage/Governance");
 const fa2Storage = require("../storage/FA2");
-const env = process.env;
+const env = require("../env");
+const { dev } = require("../scripts/sandbox/accounts");
 
 module.exports = async tezos => {
   const sender = await tezos.signer.publicKeyHash();
   governanceStorage.owner = sender;
-  if (env.NETWORK === "mainnet") {
+  if (process.env.NETWORK === "mainnet") {
     governanceStorage.token_address = env.TOKEN_ADDRESS;
   } else {
+    fa2Storage.admin = sender;
+    fa2Storage.minters = [sender];
+    fa2Storage.minters_info = [{ minter: dev.pkh, percent: 100 }];
     const deployedToken = await migrate(tezos, "FA2", fa2Storage);
     governanceStorage.token_address = deployedToken;
     console.log(`Token contract address: ${deployedToken}`);
   }
   const contractAddress = await migrate(tezos, "Governance", governanceStorage);
   console.log(`Governance contract address: ${contractAddress}`);
+  console.log(`Contracts deployed to ${env.networks[process.env.NETWORK].rpc}`);
 };

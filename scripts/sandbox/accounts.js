3a4c4ae693700d6a88c12028d3f393c307713dde
cyace84 <cyace84@gmail.com>
2021-12-18 19:41:48 +0200

    added eve acc

--- scripts/sandbox/accounts.js
+++ scripts/sandbox/accounts.js
@@ -9,6 +9,11 @@ module.exports = {
     sk: "edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt",
     pk: "edpkurPsQ8eUApnLUJ9ZPDvu98E8VNj4KtJa1aZr16Cr5ow5VHKnz4",
   },
+  eve: {
+    pkh: "tz1R2oNqANNy2vZhnZBJc8iMEqW79t85Fv7L",
+    sk: "edsk3W5Fz1yWK39sLY6vidmgkfmGAXh6V2JqUiri9W1pFeeYWbFbJL",
+    pk: "edpkujwsG5JMrWVXQwmRMBoR9yJkokRbn6wy3monpQKBpnZTs1ogRR",
+  },
   dev: {
     pkh: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
     sk: "edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq",

1f5e9f626137913040cf4fb9b8517793e4446538
cyace84 <cyace84@gmail.com>
2021-12-18 19:14:13 +0200

    added dev account for testnet

--- scripts/sandbox/accounts.js
+++ scripts/sandbox/accounts.js
@@ -9,4 +9,9 @@ module.exports = {
     sk: "edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt",
     pk: "edpkurPsQ8eUApnLUJ9ZPDvu98E8VNj4KtJa1aZr16Cr5ow5VHKnz4",
   },
+  dev: {
+    pkh: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb",
+    sk: "edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq",
+    pk: "edpkvGfYw3LyB1UcCahKQk4rF2tvbMUk8GFiTuMjL75uGXrpvKXhjn",
+  },
 };
